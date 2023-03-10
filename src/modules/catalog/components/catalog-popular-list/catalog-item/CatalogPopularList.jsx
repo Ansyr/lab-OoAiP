3536ba72d9d696085b6cfaf966efafdb4b9fa368
unknown <vasutin2003@gmail.com>
2023-03-10 19:58:44 +0300

    fixed items in api

--- src/modules/catalog/components/catalog-popular-list/catalog-item/CatalogPopularList.jsx
+++ src/modules/catalog/components/catalog-popular-list/catalog-item/CatalogPopularList.jsx
@@ -5,41 +5,41 @@ import useSWR from "swr";
 import fetcher from "../api/api";
 import { Loader } from "../../../../../components/elements/loader/Loader";
 
-const data = [
-  {
-    id: "1",
-    name: "lorem",
-    image_url:
-      "https://volgograd.karnavaltk.ru/wa-data/public/shop/products/04/webp/56/33/23356/images/36340/36340.970x0.webp",
-    price: {
-      value: 1100,
-    },
-  },
+// const data = [
+//   {
+//     id: "1",
+//     name: "lorem",
+//     image_url:
+//       "https://volgograd.karnavaltk.ru/wa-data/public/shop/products/04/webp/56/33/23356/images/36340/36340.970x0.webp",
+//     price: {
+//       value: 1100,
+//     },
+//   },
 
-  {
-    id: "2",
-    name: "lorem2",
-    image_url:
-      "https://volgograd.karnavaltk.ru/wa-data/public/shop/products/04/webp/56/33/23356/images/36340/36340.970x0.webp",
-    price: {
-      value: 1100,
-    },
-  },
-  {
-    id: "3",
-    name: "lorem3",
-    image_url:
-      "https://volgograd.karnavaltk.ru/wa-data/public/shop/products/04/webp/56/33/23356/images/36340/36340.970x0.webp",
-    price: {
-      value: 1100,
-    },
-  },
-];
+//   {
+//     id: "2",
+//     name: "lorem2",
+//     image_url:
+//       "https://volgograd.karnavaltk.ru/wa-data/public/shop/products/04/webp/56/33/23356/images/36340/36340.970x0.webp",
+//     price: {
+//       value: 1100,
+//     },
+//   },
+//   {
+//     id: "3",
+//     name: "lorem3",
+//     image_url:
+//       "https://volgograd.karnavaltk.ru/wa-data/public/shop/products/04/webp/56/33/23356/images/36340/36340.970x0.webp",
+//     price: {
+//       value: 1100,
+//     },
+//   },
+// ];
 export function CatalogPopularList(props) {
-  const { data, error, isLoading } = useSWR("http://asm-explaining.me/products", fetcher);
+  const { data, error, isLoading } = useSWR("http://asm-explaining.me/products?limit=3", fetcher);
   const { className } = props;
   
-  
+
   return (
     <section className={classNames(styles.CatalogPopularList, {}, [className])}>
       <h3>Популярное</h3>
